5c133fb8d29345095d63d1ae07664938757de457
佐藤良昭 <yoshiprograming@gmail.com>
2020-07-14 16:40:12 +0900

    アソシエーションの追加task-user

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,7 +7,7 @@ class TasksController < ApplicationController
       @task = Task.new
     end
     def create
-      @task = Task.new(task_params)
+      @task = current_user.tasks.build(task_params)
       if @task.save
         redirect_to tasks_path, notice: "追加しました！"
       else
