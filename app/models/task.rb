5535a4b2ed82800ac966d9df6599da879d436279
佐藤良昭 <yoshiprograming@gmail.com>
2020-07-17 22:28:19 +0900

    スコープ作成

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,6 @@ class Task < ApplicationRecord
     belongs_to :user
     enum importance: { 高: 0, 中: 1, 低: 2 }
     mount_uploader :image, ImageUploader
+    scope :title_search, ->(title){where("title like?", "%#{title}%")}
+    scope :importance_search, ->(importance){where(importance: importance)}
 end
